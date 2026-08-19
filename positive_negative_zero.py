c2b49170dabaaeb7ed5865ac3785359a2a0cb462
gholapaarti <gholapaarti0@gmail.com>
2026-08-19 21:19:22 +0530

    +ve,-ve,0

---
+++ positive_negative_zero.py
@@ -0,0 +1,8 @@
+#write a program to know its positive ,negative or zero
+num = int(input("Enter Number of your choice:"))
+if(num > 0):
+    print("Its postive number!")
+elif(num < 0):
+    print("Its negative number!")
+else:
+    print("its zero!")
