54d57cf724fddc7cdb3d6cf606ad559b57894871
gholapaarti <gholapaarti0@gmail.com>
2026-08-19 21:17:08 +0530

    sum

---
+++ sum_of_two_numbers.py
@@ -0,0 +1,5 @@
+#write a program to find sum of two numers using user input
+number1 = int(input("Enter first number:"))
+number2 = int(input("Enter second number:"))
+sum = number1 + number2
+print("sum is :", sum)
